d4de881d8cf2bfbb72f8ef31e5a7e57e63b79a3c
Satyam23092003 <satyam85409@gmail.com>
2025-07-09 14:28:23 +0530

    Adding Check files

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,8 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 const userRoutes = require('./routes/userRoutes');
 const ticketRoutes = require('./routes/ticketRoutes');
 
+app.use("/api/status",(req,res)=>res.send("Server is live"));
+
 app.use('/api/users', userRoutes);
 app.use('/api/tickets', ticketRoutes);
 
